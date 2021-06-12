c8ff2387b4e4cd3910e613ef6a6e25e708bd538d
katiemartinez <38431232+katiemartinez@users.noreply.github.com>
2021-06-12 14:11:58 -0400

    add language, location, ID, remove col-6

--- themes/newschool2021/views/Details/ca_objects_default_html.php
+++ themes/newschool2021/views/Details/ca_objects_default_html.php
@@ -99,41 +99,50 @@
 			
 				<hr></hr>
 					<div class="row">
-						<div class="col-sm-6">		
+						<div class="col-sm-12">		
 							{{{<ifcount code="ca_entities" min="1" max="1"><H6>Related person</H6></ifcount>}}}
 							{{{<ifcount code="ca_entities" min="2"><H6>Related people</H6></ifcount>}}}
 							{{{<unit relativeTo="ca_objects_x_entities" delimiter="<br/>"><unit relativeTo="ca_entities"><l>^ca_entities.preferred_labels</l></unit> (^relationship_typename)</unit>}}}
 							
 							{{{<ifdef code="ca_objects.wtDrawings"><H6>Work Type</H6>^ca_objects.wtDrawings<br/></ifdef>}}}
-							{{{<ifdef code="ca_objects.wtPhotographic"><H3>Work Type</H3>^ca_objects.wtPhotographic<br/></ifdef>}}}
-							{{{<ifdef code="ca_objects.wtPosters"><H3>Work Type</H3>^ca_objects.wtPosters<br/></ifdef>}}}
-							{{{<ifdef code="ca_objects.wtClippings"><H3>Work Type</H3>^ca_objects.wtClippings<br/></ifdef>}}}
-							{{{<ifdef code="ca_objects.wtPlans"><H3>Work Type</H3>^ca_objects.wtPlans<br/></ifdef>}}}
-							{{{<ifdef code="ca_objects.wtCourse"><H3>Work Type</H3>^ca_objects.wtCourse<br/></ifdef>}}}
-							{{{<ifdef code="ca_objects.wtExhibition"><H3>Work Type</H3>^ca_objects.wtExhibition<br/></ifdef>}}}
-							{{{<ifdef code="ca_objects.wtBooks"><H3>Work Type</H3>^ca_objects.wtBooks<br/></ifdef>}}}
-							{{{<ifdef code="ca_objects.wtPeriodicals"><H3>Work Type</H3>^ca_objects.wtPeriodicals<br/></ifdef>}}}
-							{{{<ifdef code="ca_objects.wtScores "><H3>Work Type</H3>^ca_objects.wtScores<br/></ifdef>}}}
-							{{{<ifdef code="ca_objects.wtScrapbooks"><H3>Work Type</H3>^ca_objects.wtScrapbooks<br/></ifdef>}}}
-							{{{<ifdef code="ca_objects.wtPhotoAlbums"><H3>Work Type</H3>^ca_objects.wtPhotoAlbums<br/></ifdef>}}}
-							{{{<ifdef code="ca_objects.wtSketchbooks"><H3>Work Type</H3>^ca_objects.wtSketchbooks<br/></ifdef>}}}
-							{{{<ifdef code="ca_objects.wtTextual"><H3>Work Type</H3>^ca_objects.wtTextual<br/></ifdef>}}}
-							{{{<ifdef code="ca_objects.wtProofs"><H3>Work Type</H3>^ca_objects.wtProofs<br/></ifdef>}}}
-							{{{<ifdef code="ca_objects.wtThree"><H3>Work Type</H3>^ca_objects.wtThree<br/></ifdef>}}}
-							{{{<ifdef code="ca_objects.wtOther"><H3>Work Type</H3>^ca_objects.wtOther<br/></ifdef>}}}
-							{{{<ifdef code="ca_objects.wtAudiovisual"><H3>Work Type</H3>^ca_objects.wtAudiovisual<br/></ifdef>}}}
+							{{{<ifdef code="ca_objects.wtPhotographic"><H6>Work Type</H6>^ca_objects.wtPhotographic<br/></ifdef>}}}
+							{{{<ifdef code="ca_objects.wtPosters"><H6>Work Type</H6>^ca_objects.wtPosters<br/></ifdef>}}}
+							{{{<ifdef code="ca_objects.wtClippings"><H6>Work Type</H6>^ca_objects.wtClippings<br/></ifdef>}}}
+							{{{<ifdef code="ca_objects.wtPlans"><H6>Work Type</H6>^ca_objects.wtPlans<br/></ifdef>}}}
+							{{{<ifdef code="ca_objects.wtCourse"><H6>Work Type</H6>^ca_objects.wtCourse<br/></ifdef>}}}
+							{{{<ifdef code="ca_objects.wtExhibition"><H6>Work Type</H6>^ca_objects.wtExhibition<br/></ifdef>}}}
+							{{{<ifdef code="ca_objects.wtBooks"><H6>Work Type</H6>^ca_objects.wtBooks<br/></ifdef>}}}
+							{{{<ifdef code="ca_objects.wtPeriodicals"><H6>Work Type</H6>^ca_objects.wtPeriodicals<br/></ifdef>}}}
+							{{{<ifdef code="ca_objects.wtScores "><H6>Work Type</H6>^ca_objects.wtScores<br/></ifdef>}}}
+							{{{<ifdef code="ca_objects.wtScrapbooks"><H6>Work Type</H6>^ca_objects.wtScrapbooks<br/></ifdef>}}}
+							{{{<ifdef code="ca_objects.wtPhotoAlbums"><H6>Work Type</H6>^ca_objects.wtPhotoAlbums<br/></ifdef>}}}
+							{{{<ifdef code="ca_objects.wtSketchbooks"><H6>Work Type</H6>^ca_objects.wtSketchbooks<br/></ifdef>}}}
+							{{{<ifdef code="ca_objects.wtTextual"><H6>Work Type</H6>^ca_objects.wtTextual<br/></ifdef>}}}
+							{{{<ifdef code="ca_objects.wtProofs"><H6>Work Type</H6>^ca_objects.wtProofs<br/></ifdef>}}}
+							{{{<ifdef code="ca_objects.wtThree"><H6>Work Type</H6>^ca_objects.wtThree<br/></ifdef>}}}
+							{{{<ifdef code="ca_objects.wtOther"><H6>Work Type</H6>^ca_objects.wtOther<br/></ifdef>}}}
+							{{{<ifdef code="ca_objects.wtAudiovisual"><H6>Work Type</H6>^ca_objects.wtAudiovisual<br/></ifdef>}}}
 					
 							{{{<ifdef code="ca_objects.measurementSet.measurements"><H6>Measurements</H6>^ca_objects.measurementSet.measurements <ifdef code="ca_objects.measurementSet.measurementsType">(^ca_objects.measurementSet.measurementsType)</ifdef></ifdef><ifdef code="ca_objects.measurementSet.measurements"> x </ifdef><ifdef code="ca_objects.measurementSet.measurements2">^ca_objects.measurementSet.measurements2 <ifdef code="ca_objects.measurementSet.measurementsType2">(^ca_objects.measurementSet.measurementsType2)</ifdef></ifdef>}}}
 					
 							{{{<ifdef code="ca_objects.descriptionSet.descriptionText"><H6>Description</H6>^ca_objects.descriptionSet.descriptionText<br/></ifdef>}}}
-							{{{<ifdef code="ca_objects.pbcoreDescription.pBdescription_text"><H3>Description</H3>^ca_objects.pbcoreDescription.pBdescription_text<br/></ifdef>}}}
-							{{{<ifdef code="ca_objects.pbcoreDescription.pBdescription_text%[pbcore_description_types=abstract]"><H3>Abstract</H3>^ca_objects.pbcoreDescription.pBdescription_text%[pbcore_description_types=abstract]<br/></ifdef>}}}
+							{{{<ifdef code="ca_objects.pbcoreDescription.pBdescription_text"><H6>Description</H6>^ca_objects.pbcoreDescription.pBdescription_text<br/></ifdef>}}}
+							{{{<ifdef code="ca_objects.pbcoreDescription.pBdescription_text%[pbcore_description_types=abstract]"><H6>Abstract</H6>^ca_objects.pbcoreDescription.pBdescription_text%[pbcore_description_types=abstract]<br/></ifdef>}}}
 					
 							{{{<ifdef code="ca_objects.inscriptionSet.inscriptionText"><H6>Inscription</H6>^ca_objects.inscriptionSet.inscriptionText<br/></ifdef>}}}
 							
 							{{{<ifcount code="ca_list_items" min="1" max="1"><H6>Related Term</H6></ifcount>}}}
 							{{{<ifcount code="ca_list_items" min="2"><H6>Related Terms</H6></ifcount>}}}
 							{{{<unit relativeTo="ca_objects_x_vocabulary_terms" delimiter="<br/>"><unit relativeTo="ca_list_items"><l>^ca_list_items.preferred_labels.name_plural</l></unit> (^relationship_typename)</unit>}}}
+							
+							{{{<ifdef code="ca_objects.containerID"><H3>Location</H3>^ca_objects.containerID<br/></ifdef>}}}
+					
+							{{{<ifdef code="ca_objects.pbcoreLanguage"><H3>Language</H3>^ca_objects.pbcoreLanguage<br/></ifdef>}}}
+
+							{{{<ifdef code="ca_objects.idno"><H3>Identifier</H3>^ca_objects.idno<br/></ifdef>}}}
+					
+							{{{<h3>Use Restrictions</h3><ifcount code="ca_collections" min="1" max="1"><unit relativeTo="ca_collections">^ca_collections.CollectionNote.NoteContent%[NoteType=conditions_governing_use]</unit></ifcount>}}}
+					
 							
 						</div><!-- end col -->				
 						<div class="col-sm-6 colBorderLeft">
