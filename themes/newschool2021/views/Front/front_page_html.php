82bee8678c79e1418812a93dc80dca1c44f641e9
katiemartinez <38431232+katiemartinez@users.noreply.github.com>
2021-06-22 13:21:21 -0400

    test basic front with kress search

--- themes/newschool2021/views/Front/front_page_html.php
+++ themes/newschool2021/views/Front/front_page_html.php
@@ -1,81 +1,54 @@
-<main id="main" role="main" class="ca ca_archive_landing">
-	<section class="intro">
-		<div class="wrap block-large">
-			<div class="wrap-max-content">
-				<div class="block-half body-text-l">
-					Fusce molestie metus metus, eget luctus ex euismod sit amet. Maecenas scelerisque lorem nisi. Proin tempus consequat maximus. Vestibulum imperdiet viverra tellus, sed semper lectus. Nullam vitae molestie purus, sed varius sem. In dictum vehicula nunc, eu viverra eros congue eu.
-				</div>
-			</div>
-		</div>
-	</section>
-	<section class="ca_nav">
-		<nav class="hide-for-mobile">	
-			<div class="wrap text-gray">
-				<form action="/Browse/archive">
-					<div class="cell text">
-						<a href="/Browse/Archive">Browse</a>
-					</div>
-					<div class="cell">
-						<label for="searchInput" class="visuallyhidden">Search the Archives</label>
-						<input id="searchInput" name="search" type="text" placeholder="Search the Archives" class="search">
-					</div>
-					<div class="cell">
-						<div class="utility-container">
-							<div class="utility utility_menu">
-								<a href="#" class="trigger">All Archival Collections</a>
-								<div class="options">
-									<a href="/Browse/collections">Collections</a>
-									<a href="/Browse/objects">Objects</a>
-									<a href="/Browse/people">People</a>
-									<a href="/Browse/organizations">Organizations</a>
-								</div>
-							</div>
-						</div>
-					</div>
-					<div class="misc">
-						<div class="cell text"><a href="/About/Index">User Guide</a></div>
-						<div class="cell text"><a href="https://archives.newschool.edu/">About<span class="long">Archives Home</span></a></div>
-					</div>
-				</form>
-			</div>
-		</nav>
-		<nav class="show-for-mobile wrap">
-			<div class="module_accordion">
-				<div class="items">
-					<div class="item" data-accordion>
-						<div class="trigger small" data-control>Archive Menu</div>
-						<div class="details" data-content style="max-height: 0px; overflow: hidden;">
-							<div class="inner">
-								<div class="module_filter_bar">
-									<div class="wrap text-gray">
-										<form action="/Browse/archive" method="post">
-											<div class="cell">
-												<input name="search" type="text" placeholder="Search the Archive" class="search">
-											</div>
-											<div class="misc">
-												<a href="/Browse/Archive">Browse</a> 
-												<a href="/About/Index">User Guide</a> 
-												<a href="https://archives.newschool.edu/">Archives Home</a>
-												<a href="/LoginReg/loginForm">Researcher Login</a> 
-											</div>
-										</form>
-									</div>
-								</div>
-							</div>
-						</div>
-					</div>
-				</div>
-			</div>
-		</nav>
-	</section>
-	<section class="block border">
-		<div class="wrap">
-			<div class="block-half text-align-center">
-				<h4 class="subheadline-bold">Highlights from the Archive</h4>
-			</div>
-		</div>
-		<?php
-				print $this->render("Front/featured_set_slideshow_html.php");
-		?>
-	</section>
-</main>
+
+/** ---------------------------------------------------------------------
+ * themes/default/Front/front_page_html : Front page of site 
+ * ----------------------------------------------------------------------
+ * CollectiveAccess
+ * Open-source collections management software
+ * ----------------------------------------------------------------------
+ *
+ * Software by Whirl-i-Gig (http://www.whirl-i-gig.com)
+ * Copyright 2013 Whirl-i-Gig
+ *
+ * For more information visit http://www.CollectiveAccess.org
+ *
+ * This program is free software; you may redistribute it and/or modify it under
+ * the terms of the provided license as published by Whirl-i-Gig
+ *
+ * CollectiveAccess is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTIES whatsoever, including any implied warranty of 
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  
+ *
+ * This source code is free and modifiable under the terms of 
+ * GNU General Public License. (http://www.gnu.org/copyleft/gpl.html). See
+ * the "license.txt" file for details, or visit the CollectiveAccess web site at
+ * http://www.CollectiveAccess.org
+ *
+ * @package CollectiveAccess
+ * @subpackage Core
+ * @license http://www.gnu.org/copyleft/gpl.html GNU Public License version 3
+ *
+ * ----------------------------------------------------------------------
+ */
+
+<div class="row" style="margin: auto;">
+	<div class="col-sm-12 col-md-10 col-md-offset-1" id="welcome-text">
+    <h3>Welcome</h3>
+    <p>On this site you can search collection guides (also called finding aids) that describe archival collections held by The New School Archives. When a new collection becomes available for research, the guide for the collection is added to this site.</p>
+  </div>
+    <hr>
+  <div>
+    <form role="search" action="<?php print caNavUrl($this->request, '', 'MultiSearch', 'Index'); ?>">
+      <div class="formOutline">
+        <div class="form-group">
+          <input type="text" class="form-control" id="heroSearchInput" placeholder="Search" name="search" autocomplete="off" aria-label="Search" />
+        </div>
+        <button type="submit" class="btn-search" id="heroSearchButton"><span class="glyphicon glyphicon-search" aria-label="<?php print _t("Submit Search"); ?>"></span></button>
+      </div>
+    </form>
+  </div>
+  <hr>
+    <h2>Highlights from the Archives</h2>
+<?php
+        print $this->render("Front/featured_set_slideshow_html.php");
+?>
+</div><!-- end row -->
