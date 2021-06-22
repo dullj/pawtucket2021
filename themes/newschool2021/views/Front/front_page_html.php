7b5566e241c6332b0c10ff5dc9d8e8b4c0d94ffd
katiemartinez <38431232+katiemartinez@users.noreply.github.com>
2021-06-22 13:06:37 -0400

    test noguchi style front

--- themes/newschool2021/views/Front/front_page_html.php
+++ themes/newschool2021/views/Front/front_page_html.php
@@ -1,78 +1,81 @@
-<?php
-/** ---------------------------------------------------------------------
- * themes/default/Front/front_page_html : Front page of site 
- * ----------------------------------------------------------------------
- * CollectiveAccess
- * Open-source collections management software
- * ----------------------------------------------------------------------
- *
- * Software by Whirl-i-Gig (http://www.whirl-i-gig.com)
- * Copyright 2013 Whirl-i-Gig
- *
- * For more information visit http://www.CollectiveAccess.org
- *
- * This program is free software; you may redistribute it and/or modify it under
- * the terms of the provided license as published by Whirl-i-Gig
- *
- * CollectiveAccess is distributed in the hope that it will be useful, but
- * WITHOUT ANY WARRANTIES whatsoever, including any implied warranty of 
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  
- *
- * This source code is free and modifiable under the terms of 
- * GNU General Public License. (http://www.gnu.org/copyleft/gpl.html). See
- * the "license.txt" file for details, or visit the CollectiveAccess web site at
- * http://www.CollectiveAccess.org
- *
- * @package CollectiveAccess
- * @subpackage Core
- * @license http://www.gnu.org/copyleft/gpl.html GNU Public License version 3
- *
- * ----------------------------------------------------------------------
- */
-		print $this->render("Front/featured_set_slideshow_html.php");
-?>
-	<div class="row" style="margin: auto;">
-		<div class="col-sm-12 col-md-10 col-md-offset-1" id="welcome-text">
-    
-    <h3>Welcome</h3>
-    <p>On this site you can search collection guides (also called finding aids) that describe archival collections held by The New School Archives. When a new collection becomes available for research, the guide for the collection is added to this site.</p>
-    <hr>
-
-    <h3>What are archival collections?</h3>
-    <p>Archival collections are groups of primary source materials created by a person, group of people, or organization in the course of their everyday work or activities. Collections can range in size from a single document to hundreds of boxes or gigabytes of digital files. They can contain a range of materials, including documents, drawings, photographs, websites, and audiovisual records. Unlike published materials, archival collections are usually unique and exist only in the collection where a researcher finds them.</p>
-    <hr>
-
-    <h3>What is not on this site?</h3>
-    <p>Fusce molestie metus metus, eget luctus ex euismod sit amet. Maecenas scelerisque lorem nisi. Proin tempus consequat maximus. Vestibulum imperdiet viverra tellus, sed semper lectus. Nullam vitae molestie purus, sed varius sem. In dictum vehicula nunc, eu viverra eros congue eu.</p>
-
-    <ul>
-      <li>
-        <p><u>Digital Collections:</u> Fusce molestie metus metus, eget luctus ex euismod sit amet. Maecenas scelerisque lorem nisi. Proin tempus consequat maximus. Vestibulum imperdiet viverra tellus, sed semper lectus. Nullam vitae molestie purus, sed varius sem. In dictum vehicula nunc, eu viverra eros congue eu.</p>
-      </li>
-
-      <li>
-        <p><u>Unprocessed Collections:</u> This site does not include collections in The New School Archives that have not yet been cataloged. If you cannot find the information you are seeking here, contact us at <a href="mailto:archives@newschool.edu">archives@newschool.edu</a> and we will gladly check our internal database for you.</p>
-      </li>
-
-      <li>
-        <p><u>Items in the Library:</u> Use The New School Library <a href="https://library.newschool.edu/" rel="noopener" target="_blank">catalog</a> to search for published library materials, such as books, journals, and commercial recordings.</p>
-      </li>
-
-      <li>
-        <p><u>Special Collections:</u> Find information about our published <a href="https://archives.newschool.edu" target="_blank" rel="noopener">Special Collections</a> holdings, such as artists' books and scores.</p>
-      </li>
-    </ul>
-      
-    <hr>
-      
-    <h3>Questions?</h3>
-      <p>Learn more about The New School Archives and Special Collections on our <a href="https://archives.newschool.edu" target="_blank" rel="noopener">homepage.</a></p>
-      <p>Contact us at <a href="mailto:archives@newschool.edu">archives@newschool.edu</a> to schedule an online appointment, request that our staff conduct research on your behalf, or for questions about The New School Archives and Special Collections.</p>
-      
-
-<?php
-
-print $this->render("Front/featured_set_slideshow_html.php");
-?>
+<main id="main" role="main" class="ca ca_archive_landing">
+	<section class="intro">
+		<div class="wrap block-large">
+			<div class="wrap-max-content">
+				<div class="block-half body-text-l">
+					Fusce molestie metus metus, eget luctus ex euismod sit amet. Maecenas scelerisque lorem nisi. Proin tempus consequat maximus. Vestibulum imperdiet viverra tellus, sed semper lectus. Nullam vitae molestie purus, sed varius sem. In dictum vehicula nunc, eu viverra eros congue eu.
+				</div>
+			</div>
 		</div>
-</div><!-- end row -->
+	</section>
+	<section class="ca_nav">
+		<nav class="hide-for-mobile">	
+			<div class="wrap text-gray">
+				<form action="/Browse/archive">
+					<div class="cell text">
+						<a href="/Browse/Archive">Browse</a>
+					</div>
+					<div class="cell">
+						<label for="searchInput" class="visuallyhidden">Search the Archives</label>
+						<input id="searchInput" name="search" type="text" placeholder="Search the Archives" class="search">
+					</div>
+					<div class="cell">
+						<div class="utility-container">
+							<div class="utility utility_menu">
+								<a href="#" class="trigger">All Archival Collections</a>
+								<div class="options">
+									<a href="/Browse/collections">Collections</a>
+									<a href="/Browse/objects">Objects</a>
+									<a href="/Browse/people">People</a>
+									<a href="/Browse/organizations">Organizations</a>
+								</div>
+							</div>
+						</div>
+					</div>
+					<div class="misc">
+						<div class="cell text"><a href="/About/Index">User Guide</a></div>
+						<div class="cell text"><a href="https://archives.newschool.edu/">About<span class="long">Archives Home</span></a></div>
+					</div>
+				</form>
+			</div>
+		</nav>
+		<nav class="show-for-mobile wrap">
+			<div class="module_accordion">
+				<div class="items">
+					<div class="item" data-accordion>
+						<div class="trigger small" data-control>Archive Menu</div>
+						<div class="details" data-content style="max-height: 0px; overflow: hidden;">
+							<div class="inner">
+								<div class="module_filter_bar">
+									<div class="wrap text-gray">
+										<form action="/Browse/archive" method="post">
+											<div class="cell">
+												<input name="search" type="text" placeholder="Search the Archive" class="search">
+											</div>
+											<div class="misc">
+												<a href="/Browse/Archive">Browse</a> 
+												<a href="/About/Index">User Guide</a> 
+												<a href="https://archives.newschool.edu/">Archives Home</a>
+												<a href="/LoginReg/loginForm">Researcher Login</a> 
+											</div>
+										</form>
+									</div>
+								</div>
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
+		</nav>
+	</section>
+	<section class="block border">
+		<div class="wrap">
+			<div class="block-half text-align-center">
+				<h4 class="subheadline-bold">Highlights from the Archive</h4>
+			</div>
+		</div>
+		<?php
+				print $this->render("Front/featured_set_slideshow_html.php");
+		?>
+	</section>
+</main>
