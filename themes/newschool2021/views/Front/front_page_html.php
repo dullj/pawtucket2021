a81ecbd5260d774acb82c5b34cb0a2bf1a7342df
katiemartinez <38431232+katiemartinez@users.noreply.github.com>
2021-06-24 10:05:55 -0400

    add user guide section

--- themes/newschool2021/views/Front/front_page_html.php
+++ themes/newschool2021/views/Front/front_page_html.php
@@ -1,16 +1,28 @@
 <div class="row" style="margin: auto;">
-  <div class="col-sm-12 col-md-10 col-md-offset-1" id="welcome-text">
+	<div class="col-sm-12 col-md-10 col-md-offset-1" id="welcome-text">
     <div class="heroSearch">
-      <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+      <p>The New School Archives Digital Collections presents <a href="https://dictionary.archivists.org/entry/born-digital.html">born-digital</a> and digitized images, text, audio and video from <a href="https://archives.newschool.edu/">The New School Archives & Special Collections</a>, home to primary source materials documenting the history of all divisions of The New School. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
       <hr>
     </div>
   </div>
 </div>
+
 <div class="row" style="margin: auto;">
   <div class="col-sm-12 col-md-10 col-md-offset-1">
     <h2 style="text-align:center; padding-bottom:20px;">Highlights from the Archives</h2>
     <?php
             print $this->render("Front/featured_set_slideshow_html.php");
     ?>
+		<hr>
   </div>
 </div><!-- end row -->
+
+<div class="row" style="margin: auto;">
+	<div class="col-sm-12 col-md-10 col-md-offset-1">
+    <div class="heroSearch">
+			    <h2 style="text-align:center; padding-bottom:20px;">Need Research Assistance?</h2>
+			<p>View our <a href="index.php/About/Index">User Guide</a> to learn tips for searching on this site and other resources to aid your research inquiry. </p>
+      <hr>
+    </div>
+  </div>
+</div>
