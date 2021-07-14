27bce9e64a49e14d9cc792fe15e24056b8c3ccd9
katiemartinez <38431232+katiemartinez@users.noreply.github.com>
2021-07-14 12:24:21 -0400

    Update access to object ID, move date below title

--- themes/newschool2021/views/Search/ca_objects_advanced_search_objects_html.php
+++ themes/newschool2021/views/Search/ca_objects_advanced_search_objects_html.php
@@ -23,19 +23,19 @@ print "<p>Enter your search terms in the fields below.</p>";
 		</div>
 	</div>
 	<div class='row'>
+		<div class="advancedSearchField col-sm-12">
+			<span class='formLabel' data-toggle="popover" data-trigger="hover" data-content="Search records of a particular date or date range.">Date range <i>(e.g. 1970-1979)</i></span>
+			{{{ca_objects.dates.dates_value%width=200px&height=40px&useDatePicker=0}}}
+		</div>
+	</div>
+	<div class='row'>
 		<div class="advancedSearchField col-sm-6">
-			<span class='formLabel' data-toggle="popover" data-trigger="hover" data-content="Search object identifiers.">Accession number</span>
+			<span class='formLabel' data-toggle="popover" data-trigger="hover" data-content="Search object identifiers.">Object Identifier</span>
 			{{{ca_objects.idno%width=210px}}}
 		</div>
 		<div class="advancedSearchField col-sm-6">
 			<span class='formLabel' data-toggle="popover" data-trigger="hover" data-content="Limit your search to object types.">Type</span>
 			{{{ca_objects.type_id%height=30px}}}
-		</div>
-	</div>
-	<div class='row'>
-		<div class="advancedSearchField col-sm-12">
-			<span class='formLabel' data-toggle="popover" data-trigger="hover" data-content="Search records of a particular date or date range.">Date range <i>(e.g. 1970-1979)</i></span>
-			{{{ca_objects.dates.dates_value%width=200px&height=40px&useDatePicker=0}}}
 		</div>
 	</div>
 	<div class='row'>
