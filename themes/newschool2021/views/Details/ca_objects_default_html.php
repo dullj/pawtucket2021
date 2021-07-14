0369735d7f8b8d88ad97c4d367d9edc206d2a915
katiemartinez <38431232+katiemartinez@users.noreply.github.com>
2021-07-14 16:01:33 -0400

    test new inscription

--- themes/newschool2021/views/Details/ca_objects_default_html.php
+++ themes/newschool2021/views/Details/ca_objects_default_html.php
@@ -117,7 +117,8 @@
 							{{{<ifdef code="ca_objects.inscriptionSet.inscriptionText"><H6>Inscription</H6></ifdef>}}}
 							{{{<unit relativeTo="ca_objects.inscriptionSet.inscriptionText" delimiter="<br/>"><unit relativeTo="ca_objects.inscriptionSet.inscriptionText"><l>^ca_objects.inscriptionSet.inscriptionText</l></unit> (^ca_objects.inscriptionSet.inscription_type1)</unit>}}}
 							
-							
+							{{{<ifdef code="ca_objects.inscriptionSet.inscriptionText"><H6>Inscription</H6></ifdef>}}}
+							{{{<unit relativeTo="ca_objects" delimiter="<br/>">^ca_objects.inscriptionSet.inscriptionText (^ca_objects.inscriptionSet.inscription_type1)</unit>}}}
 							
 												
 							{{{<ifdef code="ca_objects.pbcoreLanguage"><H6>Language</H6>^ca_objects.pbcoreLanguage<br/></ifdef>}}}
