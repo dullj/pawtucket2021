335788dfd7731b422fcd445c98a61b7b6cd38a74
katiemartinez <38431232+katiemartinez@users.noreply.github.com>
2021-07-26 13:33:03 -0400

    removew elcome text class

--- themes/newschool2021/views/Front/front_page_html.php
+++ themes/newschool2021/views/Front/front_page_html.php
@@ -1,9 +1,7 @@
 <div class="row" style="margin: auto;">
-	<div class="col-sm-12 col-md-10 col-md-offset-1" id="welcome-text">
+	<div id="welcome-text">
     <div class="heroSearch">
       <p>The New School Archives Digital Collections presents <a href="https://dictionary.archivists.org/entry/born-digital.html" rel="noopener" target="_blank">born-digital</a> and digitized images, text, audio and video from <a href="https://archives.newschool.edu/" rel="noopener" target="_blank">The New School Archives & Special Collections</a>, home to primary source materials documenting the history of all divisions of The New School.</p>
-	    <p style="text-align:center;">Need Research Assistance?</p>
-	    <p>View our <a href="index.php/About/Index">User Guide</a> to learn tips for searching on this site and other resources to aid your research inquiry. </p>
       <hr>
     </div>
   </div>
