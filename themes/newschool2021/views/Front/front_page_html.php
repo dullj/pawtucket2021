5565eb6e49edb0bc89aa12827e747547230cd7bd
katiemartinez <38431232+katiemartinez@users.noreply.github.com>
2021-07-28 14:51:53 -0400

    test section elimination

--- themes/newschool2021/views/Front/front_page_html.php
+++ themes/newschool2021/views/Front/front_page_html.php
@@ -4,4 +4,5 @@
       			<p>The New School Archives Digital Collections presents <a href="https://dictionary.archivists.org/entry/born-digital.html" rel="noopener" target="_blank">born-digital</a> and digitized images, text, audio and video from <a href="https://archives.newschool.edu/" rel="noopener" target="_blank">The New School Archives & Special Collections</a>, home to primary source materials documenting the history of all divisions of The New School.</p>
     		</div>
   	</div>
+	<hr>
 </div>
