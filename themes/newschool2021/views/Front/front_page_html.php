09188382ba8850b11c13516b411c0a7fa9999631
katiemartinez <38431232+katiemartinez@users.noreply.github.com>
2021-07-28 13:16:45 -0400

    update text intro

--- themes/newschool2021/views/Front/front_page_html.php
+++ themes/newschool2021/views/Front/front_page_html.php
@@ -1,5 +1,5 @@
 <div class="row" style="margin: auto;">
-	<div class="welcome-text">
+	<div class="welcome-text" style="font-size: 18px; line-height: 30px;">
     		<div class="heroSearch">
       			<p>The New School Archives Digital Collections presents <a href="https://dictionary.archivists.org/entry/born-digital.html" rel="noopener" target="_blank">born-digital</a> and digitized images, text, audio and video from <a href="https://archives.newschool.edu/" rel="noopener" target="_blank">The New School Archives & Special Collections</a>, home to primary source materials documenting the history of all divisions of The New School.</p>
       			<hr>
