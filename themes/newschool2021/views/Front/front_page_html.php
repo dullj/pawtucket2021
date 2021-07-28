5f40de59ed7754a35eb67e6d74c725bf566e10ed
katiemartinez <38431232+katiemartinez@users.noreply.github.com>
2021-07-28 15:18:20 -0400

    remove hr

--- themes/newschool2021/views/Front/front_page_html.php
+++ themes/newschool2021/views/Front/front_page_html.php
@@ -3,7 +3,6 @@
     		<div class="heroSearch">
       			<p>The New School Archives Digital Collections presents <a href="https://dictionary.archivists.org/entry/born-digital.html" rel="noopener" target="_blank">born-digital</a> and digitized images, text, audio and video from <a href="https://archives.newschool.edu/" rel="noopener" target="_blank">The New School Archives & Special Collections</a>, home to primary source materials documenting the history of all divisions of The New School.</p>
     		</div>
-		<hr>
   	</div>
 </div>
 
