b8310b57e4129ea089c7102bf106fc1baeb2deb3
katiemartinez <38431232+katiemartinez@users.noreply.github.com>
2021-08-03 17:27:19 -0400

    test additional set column

--- themes/newschool2021/views/Gallery/index_html.php
+++ themes/newschool2021/views/Gallery/index_html.php
@@ -43,6 +43,40 @@
 ?>
 						</ul></div><!-- end jcarousel -->
 						
+						<div class='col-sm-4'>
+					<div class="jcarousel-wrapper">
+						<!-- Carousel -->
+						<div class="jcarousel"><ul>
+<?php
+							$i = 0;
+							foreach($va_sets as $vn_set_id => $va_set){
+								if(!$vn_first_set_id){
+									$vn_first_set_id = $vn_set_id;
+								}
+								if($i == 0){
+									print "<li>";
+								}
+								$va_first_item = array_shift($va_first_items_from_set[$vn_set_id]);
+								print "<div class='galleryItem'>
+											<a href='#' onclick='jQuery(\"#gallerySetInfo\").load(\"".caNavUrl($this->request, '', 'Gallery', 'getSetInfo', array('set_id' => $vn_set_id))."\"); return false;'>
+												<div class='galleryItemImg'>".$va_first_item["representation_tag"]."</div>
+												<h5>".$va_set["name"]."</h5>
+												<p><small class='uppercase'>".$va_set["item_count"]." ".(($va_set["item_count"] == 1) ? _t("item") : _t("items"))."</small></p>
+											</a>
+											<div style='clear:both;'><!-- empty --></div>
+										</div>\n";
+								$i++;
+								if($i == 4){
+									print "</li>";
+									$i = 0;
+								}
+							}
+							if($i){
+								print "</li>";
+							}
+?>
+						</ul></div><!-- end jcarousel -->
+						
 				<div class='col-sm-8'>
 					<div id="gallerySetInfo">
 						set info here
