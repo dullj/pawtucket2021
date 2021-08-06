36ae70afef5b23a37f3ba24ea57d9725e6ae9fea
katiemartinez <38431232+katiemartinez@users.noreply.github.com>
2021-08-06 15:22:56 -0400

    Update gallery view

--- themes/newschool2021/views/Gallery/index_html.php
+++ themes/newschool2021/views/Gallery/index_html.php
@@ -1,125 +1,28 @@
-<div class="row"><div class="col-sm-12">
+<div>
 	<H1><?php print $this->getVar("section_name"); ?></H1>
 <?php
+	$o_config = caGetGalleryConfig();
+	$va_access_values = caGetUserAccessValues($this->request);
 	$va_sets = $this->getVar("sets");
 	$va_first_items_from_set = $this->getVar("first_items_from_sets");
+	print "<div class='container'>";	
 	if(is_array($va_sets) && sizeof($va_sets)){
-		# --- main area with info about selected set loaded via Ajax
-?>
-		<div class="container">
-			<div class="row">
-				<div class='col-sm-8'>
-					<div id="gallerySetInfo">
-						set info here
-					</div><!-- end gallerySetInfo -->
-				</div><!-- end col -->
-<?php
-				if(sizeof($va_sets) > 1){
-?>
-				<div class='col-sm-4'>
-					<div class="jcarousel-wrapper">
-						<!-- Carousel -->
-						<div class="jcarousel"><ul>
-<?php
-							$i = 0;
-							foreach($va_sets as $vn_set_id => $va_set){
-								if(!$vn_first_set_id){
-									$vn_first_set_id = $vn_set_id;
-								}
-								if($i == 0){
-									print "<li>";
-								}
-								$va_first_item = array_shift($va_first_items_from_set[$vn_set_id]);
-								print "<div class='galleryItem'>
-											<a href='#' onclick='jQuery(\"#gallerySetInfo\").load(\"".caNavUrl($this->request, '', 'Gallery', 'getSetInfo', array('set_id' => $vn_set_id))."\"); return false;'>
-												<div class='galleryItemImg'>".$va_first_item["representation_tag"]."</div>
-												<h5>".$va_set["name"]."</h5>
-												<p><small class='uppercase'>".$va_set["item_count"]." ".(($va_set["item_count"] == 1) ? _t("item") : _t("items"))."</small></p>
-											</a>
-											<div style='clear:both;'><!-- empty --></div>
-										</div>\n";
-								$i++;
-								if($i == 6){
-									print "</li>";
-									$i = 0;
-								}
-							}
-							if($i){
-								print "</li>";
-							}
-?>
-						</ul></div><!-- end jcarousel -->
-<?php
-						if(sizeof($va_sets) > 4){
-?>
-							<!-- Prev/next controls -->
-							<a href="#" class="galleryPrevious"><i class="fa fa-angle-left"></i></a>
-							<a href="#" class="galleryNext"><i class="fa fa-angle-right"></i></a>
-<?php
-						}
-?>
-					</div><!-- end jcarousel-wrapper -->
-					<script type='text/javascript'>
-						jQuery(document).ready(function() {		
-							/* width of li */
-							$('.jcarousel li').width($('.jcarousel').width());
-							$( window ).resize(function() { $('.jcarousel li').width($('.jcarousel').width()); });
-							/*
-							Carousel initialization
-							*/
-							$('.jcarousel')
-								.jcarousel({
-									// Options go here
-								});
-					
-							/*
-							 Prev control initialization
-							 */
-							$('.galleryPrevious')
-								.on('jcarouselcontrol:active', function() {
-									$(this).removeClass('inactive');
-								})
-								.on('jcarouselcontrol:inactive', function() {
-									$(this).addClass('inactive');
-								})
-								.jcarouselControl({
-									// Options go here
-									target: '-=1'
-								});
-					
-							/*
-							 Next control initialization
-							 */
-							$('.galleryNext')
-								.on('jcarouselcontrol:active', function() {
-									$(this).removeClass('inactive');
-								})
-								.on('jcarouselcontrol:inactive', function() {
-									$(this).addClass('inactive');
-								})
-								.jcarouselControl({
-									// Options go here
-									target: '+=1'
-								});
-								
-							
-						});
-					</script>
-				</div><!-- end col -->
-<?php
-				}else{
-					$va_first_set = array_shift($va_sets);
-					$vn_first_set_id = $va_first_set["set_id"];
-				}
-?>
-			</div><!-- end row -->
-		</div><!-- end container -->
-		<script type='text/javascript'>
-			jQuery(document).ready(function() {		
-				jQuery("#gallerySetInfo").load("<?php print caNavUrl($this->request, '*', 'Gallery', 'getSetInfo', array('set_id' => $vn_first_set_id)); ?>");
-			});
-		</script>
-<?php
+		$t_set = new ca_sets();
+		foreach($va_sets as $vn_set_id => $va_set){
+			$t_set->load($vn_set_id);
+			print "<div class='row'>";
+			print "<div class='topics-container'><div class='galleryListItem'>";
+			$va_first_item = array_shift(array_shift($t_set->getFirstItemsFromSets(array($vn_set_id), array("version" => "small", "checkAccess" => $va_access_values))));
+			print "<div class='galleryListItemImg'>".$va_first_item["representation_tag"]."</div>";
+			print "<H3>".$t_set->getLabelForDisplay()."</H3>";
+			print "<p class='count'>".$t_set->getItemCount(array("checkAccess" => $va_access_values))." items</p>";
+			if($vs_description = $t_set->get($o_config->get('gallery_set_description_element_code'))){
+				print "<p class='gallery-text'>".$vs_description."</p>";
+			}
+			print caNavLink($this->request, "<span class='glyphicon glyphicon-th-large'></span>", "", "", "Gallery", $t_set->get("set_id"))."&nbsp;&nbsp;".caNavLink($this->request, "<strong>"._t("VIEW GALLERY")."</strong>", "", "", "Gallery", $t_set->get("set_id"));
+			print "<div style='clear:both;'></div></div></div></div>\n";
+		}
 	}
+	print "</div>";
 ?>
-</div><!-- end col --></div><!-- end row -->
+</div>
