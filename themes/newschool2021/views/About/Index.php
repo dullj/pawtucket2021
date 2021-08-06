baa958abc4441a077d03f73393aa820897a00e95
katiemartinez <38431232+katiemartinez@users.noreply.github.com>
2021-08-06 14:19:59 -0400

    add links

--- themes/newschool2021/views/About/Index.php
+++ themes/newschool2021/views/About/Index.php
@@ -2,16 +2,16 @@
 	<div class="col-sm-7"><H1>User Guide</H1></div>
 	<div class="col-sm-7">
 		<h2>Welcome</h2>
-		<p>The Digital Collections contain a selection of digitized and born-digital archival material from The New School Archives. To perform a deeper search, please visit the Collection Guides (also called finding aids) where you can find descriptions of every archival collection.</p>
+		<p>The Digital Collections contain a selection of digitized and born-digital archival material from The New School Archives. To perform a deeper search, please visit the <a href="https://findingaids.archives.newschool.edu/" rel="noopener" target="_blank">Collection Guides</a> (also called finding aids) where you can find descriptions of every archival collection.</p>
 		
 		<h2>Defining Archival Collections</h2>
 		<p>Archival collections are groups of primary source materials created by a person, group of people, or organization in the course of their everyday work or activities. Collections can range in size from a single document to hundreds of boxes or gigabytes of digital files. They can contain a range of materials, including documents, drawings, photographs, websites, and audiovisual records. Unlike published materials, archival collections are usually unique and exist only in the collection where a researcher finds them.</p>
 		
 		<h2>Searching the Digital Collections</h2>
-		<p>Every item in our database holds associated metadata and PDF documents have been scanned with optical character recognition (OCR) to enable searching. You may begin your search using the top level search bar, or the Advanced Search page. Search results may be narrowed by using filters.</p>
+		<p>Every item in our database holds associated metadata and PDF documents have been scanned with optical character recognition (OCR) to enable searching. You may begin your search using the top level search bar, or the <a href="/pawtucket/index.php/Search/advanced/objects">Advanced Search</a> page. Search results may be narrowed by using filters.</p>
 		
 		<h2>Use and Citation</h2>
-		<p>To learn more about how to cite materials found in the Digital Collections, please visit the Mission and Policies section of the Archives and Special Collections main site.</p>
+		<p>To learn more about how to cite materials found in the Digital Collections, please visit the Mission and Policies section of the <a href="https://archives.newschool.edu/" rel="noopener" target="_blank">Archives and Special Collections</a> main site.</p>
 		
 		<h2>Questions or Requests?</h2>
 		<p>Contact us at archives@newschool.edu to schedule an online appointment, request that our staff conduct research on your behalf, or for questions about The New School Archives and Special Collections.</p>
