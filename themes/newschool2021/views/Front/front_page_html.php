62cc44ea99fddfb329525b5a51daade9ac8b0bc8
katiemartinez <38431232+katiemartinez@users.noreply.github.com>
2021-09-01 11:42:53 -0400

    text edit testing github connection

--- themes/newschool2021/views/Front/front_page_html.php
+++ themes/newschool2021/views/Front/front_page_html.php
@@ -1,7 +1,7 @@
 <div class="row" style="margin: auto;">
 	<div class="welcome-text" style="font-size: 18px; line-height: 30px;">
     		<div class="heroSearch">
-      			<p>The New School Archives Digital Collections presents <a href="https://dictionary.archivists.org/entry/born-digital.html" rel="noopener" target="_blank">born-digital</a> and digitized images, text, audio and video from <a href="https://archives.newschool.edu/" rel="noopener" target="_blank">The New School Archives & Special Collections</a>, home to primary source materials documenting the history of all divisions of The New School.</p>
+      			<p>The New School Archives Digital Collections present <a href="https://dictionary.archivists.org/entry/born-digital.html" rel="noopener" target="_blank">born-digital</a> and digitized images, text, audio and video from <a href="https://archives.newschool.edu/" rel="noopener" target="_blank">The New School Archives & Special Collections</a>, home to primary source materials documenting the history of all divisions of The New School.</p>
     		</div>
   	</div>
 </div>
