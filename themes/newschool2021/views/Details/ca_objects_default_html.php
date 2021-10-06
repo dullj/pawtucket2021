c5d65750912bf6db109ece3b0b0177b360f72e66
katiemartinez <38431232+katiemartinez@users.noreply.github.com>
2021-10-06 12:13:28 -0400

    test hierarchy for related collections

--- themes/newschool2021/views/Details/ca_objects_default_html.php
+++ themes/newschool2021/views/Details/ca_objects_default_html.php
@@ -78,7 +78,7 @@
 			</div><!-- end col -->
 			
 			<div class='col-sm-6 col-md-6 col-lg-5'>
-				<H4>{{{<unit relativeTo="ca_collections" delimiter=" ➔ "><l>^ca_collections.preferred_labels.name</l></unit><ifcount min="1" code="ca_collections"> ➔ </ifcount>}}}{{{ca_objects.preferred_labels.name}}}</H4>
+				<H4>{{{<unit relativeTo="ca_collections.hierarchy" delimiter=" ➔ "><l>^ca_collections.preferred_labels.name</l></unit><ifcount min="1" code="ca_collections"> ➔ </ifcount>}}}{{{ca_objects.preferred_labels.name}}}</H4>
 
 				
 				<H6>{{{<unit>^ca_objects.type_id</unit>}}}</H6>
