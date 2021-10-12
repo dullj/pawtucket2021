b04f9bd3cd71711f70669d09ae9871c8cbd1d7e8
Joshua Dull <62906500+dullj@users.noreply.github.com>
2021-10-12 17:06:19 -0400

    Add Google Analytics

--- themes/default/views/pageFormat/pageHeader.php
+++ themes/default/views/pageFormat/pageHeader.php
@@ -54,6 +54,15 @@
 	<head>
 	<meta charset="utf-8">
 	<meta name="viewport" content="width=device-width, initial-scale=1.0, minimum-scale=1.0"/>
+	<!-- Global site tag (gtag.js) - Google Analytics -->
+	<script async src="https://www.googletagmanager.com/gtag/js?id=G-G444HDG4HT"></script>
+	<script>
+	  window.dataLayer = window.dataLayer || [];
+	  function gtag(){dataLayer.push(arguments);}
+	  gtag('js', new Date());
+
+	  gtag('config', 'G-G444HDG4HT');
+	</script>
 	<?php print MetaTagManager::getHTML(); ?>
 	<?php print AssetLoadManager::getLoadHTML($this->request); ?>
 
