62063d9a3bb466f6797f5ab9c3f3e467632e2128
katiemartinez <38431232+katiemartinez@users.noreply.github.com>
2021-10-13 10:30:10 -0400

    remove related object text

--- themes/newschool2021/views/Gallery/set_item_info_html.php
+++ themes/newschool2021/views/Gallery/set_item_info_html.php
@@ -6,7 +6,4 @@
 {{{<ifdef code="ca_collections.CollectionNote.NoteContent%[NoteType=abstract]"><H3>About</H3>^ca_collections.CollectionNote.NoteContent%[NoteType=abstract]<br/></ifdef>}}}
 
 
-<?php print caDetailLink($this->request, _t("View Related Object"), '', $this->getVar("table"),  $this->getVar("row_id")); ?>
-
-<H6><?php print $this->getVar("label"); ?></H6>
 
