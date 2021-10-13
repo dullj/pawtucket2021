7db13c323fdb70d520549c819c88b6b12d12b03e
katiemartinez <38431232+katiemartinez@users.noreply.github.com>
2021-10-13 14:07:28 -0400

    update object label

--- themes/newschool2021/views/Gallery/set_item_info_html.php
+++ themes/newschool2021/views/Gallery/set_item_info_html.php
@@ -6,5 +6,5 @@
 {{{<ifdef code="ca_collections.CollectionNote.NoteContent%[NoteType=abstract]"><H3 style="padding-top:10px;">Collection Description</H3>^ca_collections.CollectionNote.NoteContent%[NoteType=abstract]<br/></ifdef>}}}
 
 
-{{{<ifdef code="ca_objects.preferred_labels.name"><H3 style="padding-top:10px;">Related Object</H3>^ca_objects.preferred_labels.name<br/><br/></ifdef>}}}
+{{{<ifdef code="ca_objects.preferred_labels.name"><H3 style="padding-top:10px;">Featured Object</H3>^ca_objects.preferred_labels.name<br/><br/></ifdef>}}}
 
