8c0666a117de61f162a8ebc781bfbd6cf3636690
katiemartinez <38431232+katiemartinez@users.noreply.github.com>
2021-10-13 11:24:46 -0400

    add object and description

--- themes/newschool2021/views/Gallery/set_item_info_html.php
+++ themes/newschool2021/views/Gallery/set_item_info_html.php
@@ -5,3 +5,12 @@
 
 {{{<ifdef code="ca_collections.CollectionNote.NoteContent%[NoteType=abstract]"><H3 style="padding-top:10px;">Collection Description</H3>^ca_collections.CollectionNote.NoteContent%[NoteType=abstract]<br/></ifdef>}}}
 
+
+{{{<ifdef code="ca_objects.preferred_labels.name"><H3 style="padding-top:10px;">Related Object</H3>^ca_objects.preferred_labels.name<br/><br/></ifdef>}}}
+
+{{{<ifdef code="ca_objects.descriptionSet.descriptionText"><H6 style="padding-top:10px;">Description</H6>^ca_objects.descriptionSet.descriptionText<br/></ifdef>}}}
+							{{{<ifdef code="ca_objects.pbcoreDescription.pBdescription_text"><H6>Description</H6>^ca_objects.pbcoreDescription.pBdescription_text<br/></ifdef>}}}
+							{{{<ifdef code="ca_objects.pbcoreDescription.pBdescription_text%[pbcore_description_types=abstract]"><H6>Abstract</H6>^ca_objects.pbcoreDescription.pBdescription_text%[pbcore_description_types=abstract]<br/></ifdef>}}}
+
+
+
