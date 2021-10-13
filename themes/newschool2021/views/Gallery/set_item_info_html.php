5e003e42d0af64d408453dfc6078bc4f799848c7
katiemartinez <38431232+katiemartinez@users.noreply.github.com>
2021-10-13 10:28:13 -0400

    Update set_item_info_html.php

--- themes/newschool2021/views/Gallery/set_item_info_html.php
+++ themes/newschool2021/views/Gallery/set_item_info_html.php
@@ -1,17 +1,12 @@
 <?php print "(".$this->getVar("set_item_num")."/".$this->getVar("set_num_items").")<br/>"; ?>
 
 
-<H4>{{{<unit relativeTo="ca_collections" delimiter=" ➔ "><l>^ca_collections.preferred_labels.name</l></unit><ifcount min="1" code="ca_collections"> ➔ </ifcount>}}}</H4>
+<H4>{{{<unit relativeTo="ca_collections" delimiter=" ➔ "><l>^ca_collections.preferred_labels.name</l></unit><ifcount min="1" code="ca_collections"></ifcount>}}}</H4>
 
-<H5>Related Object: <?php print $this->getVar("label"); ?></H5>
-
-{{{<ifdef code="ca_objects.idno"><H6>Identifer:</H6>^ca_objects.idno<br/><br/></ifdef>}}}
-
-{{{<ifdef code="ca_objects.description">^ca_objects.description<br/><br/></ifdef>}}}
-
-{{{<ifcount code="ca_entities" min="1" max="1"><b>Related person: </b></ifcount>}}}
-{{{<ifcount code="ca_entities" min="2"><b>Related people: </b></ifcount>}}}
-{{{<unit relativeTo="ca_entities" delimiter=", "><l>^ca_entities.preferred_labels.displayname</l></unit><br/><br/>}}}
+{{{<ifdef code="ca_collections.CollectionNote.NoteContent%[NoteType=abstract]"><H3>About</H3>^ca_collections.CollectionNote.NoteContent%[NoteType=abstract]<br/></ifdef>}}}
 
 
-<?php print caDetailLink($this->request, _t("VIEW RECORD"), '', $this->getVar("table"),  $this->getVar("row_id")); ?>
+<?php print caDetailLink($this->request, _t("View Related Object"), '', $this->getVar("table"),  $this->getVar("row_id")); ?>
+
+<H6><?php print $this->getVar("label"); ?></H6>
+
