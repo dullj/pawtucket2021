9a370f1ade07b18181cb50f71237d9180d53d5e5
katiemartinez <38431232+katiemartinez@users.noreply.github.com>
2021-10-13 10:57:16 -0400

    update set metadata labels

--- themes/newschool2021/views/Gallery/set_item_info_html.php
+++ themes/newschool2021/views/Gallery/set_item_info_html.php
@@ -3,7 +3,7 @@
 
 <H4>{{{<unit relativeTo="ca_collections" delimiter=" ➔ "><l>^ca_collections.preferred_labels.name</l></unit><ifcount min="1" code="ca_collections"></ifcount>}}}</H4>
 
-{{{<ifdef code="ca_collections.CollectionNote.NoteContent%[NoteType=abstract]"><H3 style="padding-top:10px;">Collection Description </H3>^ca_collections.CollectionNote.NoteContent%[NoteType=abstract]<br/></ifdef>}}}
+{{{<ifdef code="ca_collections.CollectionNote.NoteContent%[NoteType=abstract]"><H3 style="padding-top:10px;">Description of Related Collection</H3>^ca_collections.CollectionNote.NoteContent%[NoteType=abstract]<br/></ifdef>}}}
 
 <?php print caDetailLink($this->request, _t("VIEW RELATED OBJECT"), '', $this->getVar("table"),  $this->getVar("row_id")); ?>
 
