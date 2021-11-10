1e5ce3b8285ee7a32c2b70c00347df3b3f696cdf
katiemartinez <38431232+katiemartinez@users.noreply.github.com>
2021-11-10 12:12:08 -0500

    remove subjects

--- themes/newschool2021/views/Details/ca_objects_default_html.php
+++ themes/newschool2021/views/Details/ca_objects_default_html.php
@@ -134,9 +134,7 @@
 							{{{<ifdef code="ca_objects.pbcoreLanguage"><H6>Language</H6>^ca_objects.pbcoreLanguage<br/></ifdef>}}}
 							
 							{{{<ifcount code="ca_list_items" min="1" max="1"><H6>Related Repository</H6></ifcount>}}}
-							
-							{{{<ifdef code="ca_objects.LcshTopical"><H6>Subjects</H6>^ca_objects.LcshTopical<br/></ifdef>}}}
-							
+														
 							
 <?php
     if (is_array($terms = $t_object->get('ca_list_items.preferred_labels.name_plural', ['returnAsArray' => true])) && sizeof($terms)) {
