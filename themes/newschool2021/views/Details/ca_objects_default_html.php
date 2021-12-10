2c248e8bd8f62c4afc23c39ac7c2acdffa4f0686
katiemartinez <38431232+katiemartinez@users.noreply.github.com>
2021-12-10 11:28:33 -0500

    test subject term

--- themes/newschool2021/views/Details/ca_objects_default_html.php
+++ themes/newschool2021/views/Details/ca_objects_default_html.php
@@ -139,9 +139,9 @@
 				if(is_array($va_list_items) && sizeof($va_list_items)){
 					$va_terms = array();
 					foreach($va_list_items as $va_list_item){
-						$va_terms[] = caNavLink($this->request, $va_list_item["name_singular"], "", "", "Browse", "objects", array("facet" => "repository_facet", "id" => $va_list_item["item_id"]));
+						$va_terms[] = caNavLink($this->request, $va_list_item["name_singular"], "", "", "Browse", "objects", array("facet" => "term_facet", "id" => $va_list_item["item_id"]));
 					}
-					print "<div class='unit'><H6>Related Subjects".((sizeof($va_terms) > 1) ? "s" : "")."</H6>".join($va_terms, ", ")."</div>";	
+					print "<div class='unit'><H6>Subject".((sizeof($va_terms) > 1) ? "s" : "")."</H6>".join($va_terms, ", ")."</div>";	
 				}
 ?>
 																					
