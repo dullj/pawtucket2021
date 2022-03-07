2b40b2013863994de1bd3e201cb8655eb51f6f2c
katiemartinez <38431232+katiemartinez@users.noreply.github.com>
2022-03-07 12:38:11 -0500

    remove finding aids in header

--- themes/newschool2021/views/pageFormat/pageHeader.php
+++ themes/newschool2021/views/pageFormat/pageHeader.php
@@ -137,8 +137,7 @@
 ?>
 			<div class="col-md-12">
       				<h1 id="fullsitetitle">The New School Archives And Special Collections</h1>
-      				<h2 id="site-title"><a href="/index.php">Digital Archives </a> 
-				<a href="https://findingaids.archives.newschool.edu/" rel="noopener" target="_blank" id="findingaids">/ Finding Aids</a></span></h2>
+      				<h2 id="site-title"><a href="/index.php">Digital Archives </a> </h2>
     			</div>
 			
 			<div class="collapse navbar-collapse" id="bs-main-navbar-collapse-1">
