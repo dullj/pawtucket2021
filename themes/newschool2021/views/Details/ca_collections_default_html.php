8ceff4f16adef4d016b01bc5be0db6875ddf5b90
katiemartinez <38431232+katiemartinez@users.noreply.github.com>
2022-03-11 10:11:12 -0500

    remove large single related object

--- themes/newschool2021/views/Details/ca_collections_default_html.php
+++ themes/newschool2021/views/Details/ca_collections_default_html.php
@@ -60,7 +60,7 @@
 					
 					{{{<ifdef code="ca_collections.idno"><H3>Collection Identifier</H3>^ca_collections.idno</ifdef>}}}
 					
-					{{{<ifcount code="ca_objects" min="1" max="1"><div class='unit'><unit relativeTo="ca_objects" delimiter=" "><l>^ca_object_representations.media.large</l><div class='caption'>Related Object: <l>^ca_objects.preferred_labels.name</l></div></unit></div></ifcount>}}}
+					
 <?php
 				# Comment and Share Tools
 				if ($vn_comments_enabled | $vn_share_enabled) {
